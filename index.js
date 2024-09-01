c5eb3a40b43aba1c116f98eec307e71b9e04c5c9
xyz-mail1 <sunday.september@protonmail.com>
2024-09-01 20:56:23 +0530

    add expressjs for render

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 require("module-alias/register");
-
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 10000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 if (!process.env.token) {
   throw new Error("Bot token is not set");
 }
